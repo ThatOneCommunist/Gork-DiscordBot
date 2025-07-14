00a859196203684706ed3c227e75803c9483b93d
Malachi McArthur <malachi0mcarthur@gmail.com>
2025-07-14 02:01:53 -0500

    fixed switch case started on reactions
    funny things happening

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const { generalPrompt } =  require('./generalPrompts.js');
-const { questionprompts} = require('./questionPrompts.js');
+const { questionprompts } = require('./questionPrompts.js');
 const { Client, IntentsBitField } = require('discord.js');
 var botId = new String();
+// What Gork can 
 const client = new Client({ 
     intents: [
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.GuildMessageReactions,
         IntentsBitField.Flags.MessageContent,
-        IntentsBitField.Flags.DirectMessages
+        IntentsBitField.Flags.DirectMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildVoiceStates,
+        IntentsBitField.Flags.GuildExpressions,
+        IntentsBitField.Flags.GuildIntegrations
+        
 ]});
 client.on('ready', (c)=> {
     console.log(c.user.tag+' is gooning');
@@ -19,27 +25,41 @@ client.on('ready', (c)=> {
 // random message if he is @ed and asked is this true
 client.on('messageCreate', (msg)=>{
     console.log(msg.content);
-    if (msg.content.toLowerCase().includes((botId+" is this true")) || 
-    msg.content.toLowerCase().includes((botId+" is this fake")) || msg.content.toLowerCase().includes((botId+" is this false"))){
-        msg.reply(questionprompts[Math.floor(Math.random()*questionprompts.length)]);
-        timing = 2147483646
+    switch (true){
+        case(msg.content.toLowerCase().includes((botId+" is this true"))) || 
+        (msg.content.toLowerCase().includes(botId+" is this fake")) || 
+        (msg.content.toLowerCase().includes(botId+" is this false")) :
+            msg.reply(questionprompts[Math.floor(Math.random()*questionprompts.length)]);
+            timing = 2147483646
+            break;
+        
+        case(msg.content.includes("WHAT DID I SACRIFICE")):
+            msg.reply("https://tenor.com/view/luthen-rael-luthen-rael-star-wars-andor-gif-5644261169390854326");
+            timing = 2147483646
+            break;
+        
+        case(msg.content.toLowerCase().includes("gabe is a furry")):
+            msg.reply("ON MY MOMMA THATS TRUE");
+            timing = 2147483646
+            break;
+            
+        case(msg.content.includes(botId)):
+            msg.reply(generalPrompt[Math.floor(Math.random()*generalPrompt.length)]);
+            timing = 2147483646
+            break;
+
+        default:
+             break;
     }
-    // More general whenever gork is @ed
-    else if (msg.content.includes(botId)) {
-        msg.reply(generalPrompt[Math.floor(Math.random()*generalPrompt.length)]);
-        timing = 2147483646
-    }
-    else if (msg.content.includes("WHAT DID I SACRIFICE")){
-        msg.reply("https://tenor.com/view/luthen-rael-luthen-rael-star-wars-andor-gif-5644261169390854326");
-        timing = 2147483646
-    }
-    else if (msg.content.toLowerCase().includes("gabe is a furry")){
-        msg.reply("ON MY MOMMA THATS TRUE");
-        timing = 2147483646
-    }
-    setInterval(msg.reply("WHERES MY "+botId+"ing"),timing);
 });
 
+client.on('messageReactionAdd', (react)=>{
+    console.log(react.emoji.name);
+    switch (react.emoji.name){
+        case(react.emoji.name):
+        break;
+    }
+})
 
 // Where to put the Token
 client.login("");
