499fa28fcd4a6f47bc1478a73d3719530dda0337
Malachi McArthur <malachi0mcarthur@gmail.com>
2025-08-04 18:59:43 -0500

    NONTESTED TTS UPDATE
    Could be the most massive inclussion to gork with the funniest addition yet

--- src/Events/commands/Funny/TextToSpeach.js
+++ src/Events/commands/Funny/TextToSpeach.js
@@ -1,8 +1,18 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 const { client } = require('../../../util/client');
-const { TextToSpeechClient } = require('@google-cloud/text-to-speech');
 const util = require('util');
 const fs = require('fs')
+const TextToSpeech = require('@google-cloud/text-to-speech');
+const { AudioPlayer, createAudioPlayer, NoSubscriberBehavior, createAudioResource } = require('@discordjs/voice');
+const { joinVoiceChannel } = require('@discordjs/voice');
+require('./../../../../serviceaccount.json')
+
+const player = createAudioPlayer({
+  behaviors:{
+    noSubscriber: NoSubscriberBehavior.Pause,
+  }
+})
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -15,25 +25,36 @@ module.exports = {
         ),
 	async execute(interaction) {
 		try {
-			convertTextToMp3(interaction.options.getString("message"),'placeholder')
+			convertTextToMp3(interaction.options.getString("message"),'tts.mp3')
+      const resource = createAudioResource('tts.mp3');
+      const connection = joinVoiceChannel({
+        channelId: interaction.member.voicechannel.id,
+        guildId: interaction.guild.id,
+        adapterCreator: interaction.guild.voiceAdapterCreator,
+      });
+      player.play(resource)
+      connection.subscribe(player)
 			await interaction.reply({content:`${interaction.options.getString("message")}`,flags:MessageFlags.Ephemeral});
 		} catch (error) {
 			console.log(error);
+      interaction.reply("I want to kill myself");
 		}
 	},
 };
 
-    async function convertTextToMp3(text, outputPath) {
-      const ttsclient = new TextToSpeechClient();
+async function convertTextToMp3(text, outputPath) {
+    const ttsclient = new TextToSpeech.TextToSpeechClient
+    const request = {
+      input: { text: text },
+      voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+      audioConfig: { audioEncoding: 'MP3' },
+    };
+  
+    const [response] = ttsclient.synthesizeSpeech(request);
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile(outputPath, response.audioContent, 'binary');
+    console.log(`Audio content written to file: ${outputPath}`);
+}
 
-      const request = {
-        input: { text: text },
-        voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
-        audioConfig: { audioEncoding: 'MP3' },
-      };
-	  
-      const [response] = await ttsclient.synthesizeSpeech(request);
-      const writeFile = util.promisify(fs.writeFile);
-      await writeFile(outputPath, response.audioContent, 'binary');
-      console.log(`Audio content written to file: ${outputPath}`);
-    }
+
+
--- src/util/connection.js
+++
@@ -1,7 +0,0 @@
-const { joinVoiceChannel } = require('@discordjs/voice');
-
-module.exports.connection = joinVoiceChannel({
-	channelId: channel.id,
-	guildId: channel.guild.id,
-	adapterCreator: channel.guild.voiceAdapterCreator,
-});
