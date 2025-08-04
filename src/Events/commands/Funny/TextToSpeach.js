8310254e11c835c12cb960ef0b7c339e513990ef
Malachi McArthur <malachi0mcarthur@gmail.com>
2025-08-04 16:44:03 -0500

    google cloud

--- src/Events/commands/Funny/TextToSpeach.js
+++ src/Events/commands/Funny/TextToSpeach.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 const { client } = require('../../../util/client');
+const { TextToSpeechClient } = require('@google-cloud/text-to-speech');
+const util = require('util');
+const fs = require('fs')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,9 +15,25 @@ module.exports = {
         ),
 	async execute(interaction) {
 		try {
+			convertTextToMp3(interaction.options.getString("message"),'placeholder')
 			await interaction.reply({content:`${interaction.options.getString("message")}`,flags:MessageFlags.Ephemeral});
 		} catch (error) {
 			console.log(error);
 		}
 	},
 };
+
+    async function convertTextToMp3(text, outputPath) {
+      const ttsclient = new TextToSpeechClient();
+
+      const request = {
+        input: { text: text },
+        voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+        audioConfig: { audioEncoding: 'MP3' },
+      };
+	  
+      const [response] = await ttsclient.synthesizeSpeech(request);
+      const writeFile = util.promisify(fs.writeFile);
+      await writeFile(outputPath, response.audioContent, 'binary');
+      console.log(`Audio content written to file: ${outputPath}`);
+    }
